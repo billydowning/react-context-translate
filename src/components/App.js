72f439afc728b642e118e47f4b36d0436faf2d3c
Billy Downing <wldowning@gmail.com>
2020-11-16 15:10:46 -0700

    added Consumer to get data from Context

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class App extends React.Component {
   state = { language: 'english' };
@@ -17,9 +18,11 @@ class App extends React.Component {
         <i className="flag us" onClick={() => this.onLanguageChange('english')} />
         <i className="flag de" onClick={() => this.onLanguageChange('german')} />
       </div>
-      <LanguageContext.Provider value={this.state.language} >
-        <UserCreate />
-      </LanguageContext.Provider>
+      <ColorContext.Provider value="red">
+        <LanguageContext.Provider value={this.state.language} >
+          <UserCreate />
+        </LanguageContext.Provider>
+      </ColorContext.Provider>
     </div>
     );
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
-  static contextType = LanguageContext;
+
+  //static contextType = LanguageContext;
   //this is how to hook up a context object to a class component
+  //only needed when using this.context, not with Consumer
+  //Should use Consumer when pulling data from multiple context objects
+  //Can use this.context when only pulling from one context object
 
   render(){
-    const text = this.context === 'english' ? 'Submit' : 'Vorlegen'
-    return <button className="ui button primary">{text}</button>
+    //const text = this.context === 'english' ? 'Submit' : 'Vorlegen'
+    return (
+      <ColorContext.Consumer>
+        {(color) =>
+          <button className={`ui button ${color}`}>
+          <LanguageContext.Consumer>
+            {(value) => value === 'english' ? 'Submit' : 'Vorlegen'}
+          </LanguageContext.Consumer>
+          </button>
+        }
+      </ColorContext.Consumer>
+    );
   }
 }
 
